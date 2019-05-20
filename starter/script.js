78192fa6fe1027f1544016b61c89982fd778a6f0
Meghna Srivastava <meghna.srivastava@auto1.com>
2019-05-20 17:23:27 +0200

    solved problem with array manipulation

--- starter/script.js
+++ starter/script.js
@@ -42,7 +42,7 @@ GOOD LUCK 😀
 */
 
 
-var scoreJohn = (10 + 10+ 10) / 3;
+var scoreJohn = (10 + 10 + 10) / 3;
 var scoreMike = (10 + 10 + 10) / 3;
 var scoreMary = (97 + 134 + 105) / 3;
 console.log(scoreJohn, scoreMike, scoreMary);
@@ -83,15 +83,53 @@ console.log(ageJohn, ageMike, ageJane);
 function yearsUntilRetirement(year, firstName) {
     var age = calculateAge(year);
     var retirement = 65 - age;
-    
+
     if (retirement > 0) {
         console.log(firstName + ' retires in ' + retirement + ' years.');
     } else {
         console.log(firstName + ' is already retired.')
-    }   
+    }
 }
 
 yearsUntilRetirement(1990, 'John');
 yearsUntilRetirement(1948, 'Mike');
 yearsUntilRetirement(1969, 'Jane');
 
+
+/*****************************
+* CODING CHALLENGE 3
+*/
+
+/*
+John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+*/
+
+function tipCalculator(bill) {
+    var percentage;
+    if (bill < 50) {
+        percentage = .2;
+    } else if (bill >= 50 && bill < 200) {
+        percentage = .15;
+    } else {
+        percentage = .1;
+    }
+    return percentage * bill;
+}
+
+var bills = [124, 48, 268];
+var tips = [
+    tipCalculator(bills[0]),
+    tipCalculator(bills[1]),
+    tipCalculator(bills[2])
+];
+var finalValues = [
+    bills[0] + tips[0],
+    bills[1] + tips[1], 
+    bills[2] + tips[2]
+];
+console.log(tips, finalValues);
